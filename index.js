58014d7c968e2636bf352afda5fad54570f4da71
Mel Marie Resurreccion <melmarieresurreccion@Mels-MacBook-Air.local>
2023-09-05 10:58:33 +0800

    add input

--- index.js
+++ index.js
@@ -16,3 +16,15 @@ if (status == "no"){
 let food = prompt("what is your favorite food?");
 alert("I like" + food + "too!"); 
 }
+
+function validate(e) {
+    // console.log(e.value);
+    let text = e.value;
+    let remaining_characters = 150;
+    document.getElementById("remaining").innerHTML = remaining_characters - text.length;
+    // console.log(text.length)
+    if (text.length === 150){
+        alert("you have reach the limit.");
+    }
+     
+}
